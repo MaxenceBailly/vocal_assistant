d37e0b5cd6131b8b782aa075d190cb95271a1e3d
MaxenceBailly <maxence.bailly.fr@gmail.com>
2021-04-05 12:46:10 +0200

    switch windows and linux

--- talk.py
+++ talk.py
@@ -1,10 +1,12 @@
-import pyttsx3
+import platform
+from time import sleep
+import os
 
-engine = pyttsx3.init()
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
-newVoiceRate = 130
-engine.setProperty('rate',newVoiceRate)
+if platform.system() == 'Windows':
+    import pyttsx3
+else:
+    from gtts import gTTS
+    import pyglet
 
 def talk(text):
     """talk function
@@ -12,6 +14,23 @@ def talk(text):
     Args:
         text (string): text to say
     """
-    print(f"--> {text}")
-    engine.say(text)
-    engine.runAndWait()
+    if platform.system() == 'Windows':
+        engine = pyttsx3.init()
+        voices = engine.getProperty("voices")
+        engine.setProperty("voice", voices[1].id)
+        newVoiceRate = 130
+        engine.setProperty('rate',newVoiceRate)
+        engine.say(text)
+        engine.runAndWait()
+    else:
+        tts = gTTS(text=text, lang='en')
+        filename = '/tmp/temp.mp3'
+        tts.save(filename)
+
+        music = pyglet.media.load(filename, streaming=False)
+        music.play()
+
+        sleep(music.duration) #prevent from killing
+        os.remove(filename) #remove temperory file
+
+talk("hello")
